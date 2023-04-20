d57367796bb4d5d8d51d11c1530dbc40a84ac5d8
michellesambuu <michelle.sambuu@gmail.com>
2023-04-20 11:19:10 -0500

    added postroutes to add new notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,5 +1,6 @@
 const router=require("express").Router()
 const fs=require("fs")
+const uniqid=require('uniqid')
 
 router.get("/api/notes",(req,res)=>{
     fs.readFile("db/db.json","utf-8",(err,data)=>{
@@ -7,4 +8,18 @@ router.get("/api/notes",(req,res)=>{
         return res.json(JSON.parse(data))
     })
 })
+
+router.post("/api/notes",(req,res)=>{
+const dbData=JSON.parse(fs.readFileSync('db/db.json'))
+const newNote=req.body
+const id='id'
+const noteId=uniqid()
+newNote[id]=noteId
+dbData.push(newNote)
+fs.writeFile('db/db.json',JSON.stringify(dbData),(err)=>{
+    if(err) throw err
+    res.json(dbData) 
+})
+})
+
 module.exports=router
