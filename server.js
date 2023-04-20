30e4311ff1ff239be533856d58908d5dec1f6d4b
michellesambuu <michelle.sambuu@gmail.com>
2023-04-20 10:50:01 -0500

    added home routes and connected them to the server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const homeRoutes=require('./routes/homeroutes')
 const PORT = process.env.PORT||3001;
 
 const app = express();
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+app.use(homeRoutes)
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
